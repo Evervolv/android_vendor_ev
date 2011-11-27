2505263b85ae53e9b7d771a725870627ab776a9f
Koushik Dutta <koushd@gmail.com>
2011-11-26 18:51:42 -0800

    (squashed) roomservice
    This is a combination of 10 commits.
    The first commit's message is:
    roomservice delivers you lunch combos from the CyanogenMod github.
    
    This is the 2nd commit message:
    
    fix roomservice formatting
    
    This is the 3rd commit message:
    
    support product names with _ in them
    
    This is the 4th commit message:
    
    fix roomservice to handle pagination
    
    Change-Id: I4923c2f768094dbad8e06a72d9f27d46414030ab
    
    This is the 5th commit message:
    
    roomservice: Add lightweight dependencies to repositories
    
    Roomservice can already fetch your cm_<device> without the need for a
    manifest entry.
    
    However, when working with common repositories, there is no way of
    actually fetching them without adding to the manifest. This patch
    introduces a lightweight dependency system. Each repository can have a
    cm.dependencies in the following json format:
    
    [
      {
        "repository": "repository_name_on_cm_organization"
        "target_path": "target/path"
      },
      ...
    ]
    
    For instance, for cm_anzu I need android_device_semc_msm7x30-common and
    android_device_semc_mogami-common. I would add both to cm.dependencies
    as follows:
    
    [
      {
        "repository": "android_device_semc_msm7x30-common",
        "target_path": "device/semc/msm7x30-common"
      },
      {
        "repository": "android_device_semc_mogami-common",
        "target_path": "device/semc/mogami-common"
      }
    ]
    
    Roomservice would then fetch the anzu repository, parse the dependency
    files and add/fetch/sync these additional repositories if they don't
    exist already.
    
    This also adds pretty printing to the output xml.
    
    Change-Id: I9cc847adfc717a06439bc6094213ed6492343158
    
    This is the 6th commit message:
    
    roomservice: Add branch support to cm.dependencies
    
    Allow the cm.dependencies entries to provide an optional "branch" for
    the repository dependencies. Added to fully support
    http://wiki.cyanogenmod.com/wiki/Integrated_kernel_building
    
    Change-Id: I35b51920d296afa329411af6172c7bd9aeef4af8
    
    This is the 7th commit message:
    
    roomservice: Fill in dependencies for already-deployed repositories
    
    Change-Id: I01fd408c9c4bfa78097c7f848b2556d2b2b180f3
    
    This is the 8th commit message:
    
    roomservice: Extend dependency-checks to devices in main manifest
    
    CM currently keeps devices inherited from AOSP in the main manifest,
    so take that into account as well when checking device paths
    
    Change-Id: I9663f283617f237428b4eaa0cd60b5de2b86a7b9
    
    This is the 9th commit message:
    
    roomservice: fixup to fetch from evervolvs github
    
    now reads from ev.dependencies to build local_manifest
    
    Change-Id: I3c468e44164bf0ff50d87c3a5dca3b99eed1d78e
    
    This is the 10th commit message:
    
    roomservice: dont auto fetch deps
    
    since we will only be using roomservice to fetch kernel repos in is
    not a good idea to run it with lunch, instead add find_deps to manually
    fetch the kernel repo for selected device
    
    Change-Id: Id3b092f2bd2775d4f1af6b9dcd3cc52d16300b4c
    Signed-off-by: Andrew Sutherland <dr3wsuth3rland@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -2,4 +2,18 @@ function __print_ev_functions_help() {
 cat <<EOF
 Additional Evervolv functions:
 EOF
+
+function find_deps() {
+
+    if [ -z "$TARGET_PRODUCT" ]
+    then
+        echo "TARGET_PRODUCT not set..."
+        lunch
+    fi
+
+    vendor/ev/build/tools/roomservice.py $TARGET_PRODUCT true
+    if [ $? -ne 0 ]
+    then
+        echo "find_deps failed."
+    fi
 }
---
+++ build/tools/roomservice.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+import os
+import sys
+import urllib2
+import json
+import re
+from xml.etree import ElementTree
+
+product = sys.argv[1];
+
+if len(sys.argv) > 2:
+    depsonly = sys.argv[2]
+else:
+    depsonly = None
+
+device = product[product.index("_") + 1:]
+if not depsonly:
+    print "Device %s not found. Attempting to retrieve device repository from Evervolv Github (http://github.com/Evervolv)." % device
+
+repositories = []
+
+page = 1
+while not depsonly:
+    result = json.loads(urllib2.urlopen("http://github.com/api/v2/json/repos/show/Evervolv?page=%d" % page).read())
+    if len(result['repositories']) == 0:
+        break
+    repositories = repositories + result['repositories']
+    page = page + 1
+
+def exists_in_tree(lm, repository):
+    for child in lm.getchildren():
+        if child.attrib['name'].endswith(repository):
+            return True
+    return False
+
+# in-place prettyprint formatter
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
+def get_from_manifest(devicename):
+    try:
+        lm = ElementTree.parse(".repo/local_manifest.xml")
+        lm = lm.getroot()
+    except:
+        lm = ElementTree.Element("manifest")
+
+    for localpath in lm.findall("project"):
+        if re.search("android_device_.*_%s$" % device, localpath.get("name")):
+            return localpath.get("path")
+
+    # Devices originally from AOSP are in the main manifest...
+    try:
+        mm = ElementTree.parse(".repo/manifest.xml")
+        mm = mm.getroot()
+    except:
+        mm = ElementTree.Element("manifest")
+
+    for localpath in mm.findall("project"):
+        if re.search("android_device_.*_%s$" % device, localpath.get("name")):
+            return localpath.get("path")
+
+    return None
+
+def is_in_manifest(projectname):
+    try:
+        lm = ElementTree.parse(".repo/local_manifest.xml")
+        lm = lm.getroot()
+    except:
+        lm = ElementTree.Element("manifest")
+
+    for localpath in lm.findall("project"):
+        if localpath.get("name") == projectname:
+            return 1
+
+    return None
+
+def add_to_manifest(repositories):
+    try:
+        lm = ElementTree.parse(".repo/local_manifest.xml")
+        lm = lm.getroot()
+    except:
+        lm = ElementTree.Element("manifest")
+
+    for repository in repositories:
+        repo_name = repository['repository']
+        repo_target = repository['target_path']
+        if exists_in_tree(lm, repo_name):
+            print '%s already exists' % (repo_name)
+            continue
+
+        print 'Adding dependency: %s -> %s' % (repo_name, repo_target)
+        project = ElementTree.Element("project", attrib = { "path": repo_target,
+            "remote": "github", "name": "%s" % repo_name })
+
+        if 'branch' in repository:
+            project.set('revision',repository['branch'])
+
+        lm.append(project)
+
+    indent(lm, 0)
+    raw_xml = ElementTree.tostring(lm)
+    raw_xml = '<?xml version="1.0" encoding="UTF-8"?>\n' + raw_xml
+
+    f = open('.repo/local_manifest.xml', 'w')
+    f.write(raw_xml)
+    f.close()
+
+def fetch_dependencies(repo_path):
+    print 'Looking for dependencies'
+    dependencies_path = repo_path + '/ev.dependencies'
+    syncable_repos = []
+
+    if os.path.exists(dependencies_path):
+        dependencies_file = open(dependencies_path, 'r')
+        dependencies = json.loads(dependencies_file.read())
+        fetch_list = []
+
+        for dependency in dependencies:
+            if not is_in_manifest("%s" % dependency['repository']):
+                fetch_list.append(dependency)
+                syncable_repos.append(dependency['target_path'])
+
+        dependencies_file.close()
+
+        if len(fetch_list) > 0:
+            print 'Adding dependencies to manifest'
+            add_to_manifest(fetch_list)
+    else:
+        print 'Dependencies file not found, bailing out.'
+
+    if len(syncable_repos) > 0:
+        print 'Syncing dependencies'
+        os.system('repo sync %s' % ' '.join(syncable_repos))
+
+if depsonly:
+    repo_path = get_from_manifest(device)
+    if repo_path:
+        fetch_dependencies(repo_path)
+    else:
+        print "Trying dependencies-only mode on a non-existing device tree?"
+
+    sys.exit()
+
+else:
+    for repository in repositories:
+        repo_name = repository['name']
+        if repo_name.startswith("android_device_") and repo_name.endswith("_" + device):
+            print "Found repository: %s" % repository['name']
+            manufacturer = repo_name.replace("android_device_", "").replace("_" + device, "")
+
+            repo_path = "device/%s/%s" % (manufacturer, device)
+
+            add_to_manifest([{'repository':repo_name,'target_path':repo_path}])
+
+            print "Syncing repository to retrieve project."
+            os.system('repo sync %s' % repo_path)
+            print "Repository synced!"
+
+            fetch_dependencies(repo_path)
+            print "Done"
+            sys.exit()
+
+print "Repository for %s not found in the Evervolv Github repository list. If this is in error, you may need to manually add it to your local_manifest.xml." % device
