5ae1fb1d003a2db7d3681d1bd8d0c244bdbcdcdf
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-12-20 01:24:56 -0500

    remove duplicated ringtones (ev part)
    Change-Id: Iadff7b73615fcb7d09922db29b771393f4d0d6e6

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -3,7 +3,6 @@ $(call inherit-product, vendor/ev/config/common.mk)
 
 # Bring in all audio files
 include frameworks/base/data/sounds/AllAudio.mk
-include frameworks/base/data/sounds/AudioPackage7.mk
 
 # Default ringtone --TODO: Change these.
 PRODUCT_PROPERTY_OVERRIDES += \
