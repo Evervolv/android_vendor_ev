9e9aeeb413ba2b068317e795b6040ad066ce030e
andrew.boren <andrew.boren@gmail.com>
2011-12-21 18:51:34 -0700

    Add tenderloing (HP Touchpad) to build.
    Change-Id: I3ea5c4f50ebe289b8c9384f345506769edbe5a88

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,4 +5,5 @@ add_lunch_combo ev_passion-eng
 add_lunch_combo ev_shooter-eng
 add_lunch_combo ev_speedy-eng
 add_lunch_combo ev_supersonic-eng
+add_lunch_combo ev_tenderloin-eng
 add_lunch_combo ev_vivow-eng
