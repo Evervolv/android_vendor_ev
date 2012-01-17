92fd0dc14f12632e032663eae9e4d93a543ec7b7
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-01-16 23:30:32 -0600

    Revert "gapps: add more overrides"
    This reverts commit b80c83137590948ac7738c1cafe25487ab8724b2.
    
    its better this way.

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 # We need a way to prevent the stuff Google Apps replaces from being included in the build.
 # This is a hacky way to do that.
 ifeq ($(GAPPS),true)
-    PACKAGES.Email.OVERRIDES := Provision QuickSearchBox Calendar CalendarProvider libspeexresampler
+    PACKAGES.Email.OVERRIDES := Provision QuickSearchBox
 endif
