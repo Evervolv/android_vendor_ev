920c75e376f67dc02551eeee0aaf12c2ea40a78f
Paul Mongold <pmongold@gmail.com>
2012-01-20 00:18:45 -0500

    gapps: update apps
      * exchange mail now syncs with source-built Exchange.apk but not cal
    
    Change-Id: I4f04fd4709e37356d49c193c6b130d6df6ff2105

--- prebuilt/gapps/gapps.mk
+++ prebuilt/gapps/gapps.mk
@@ -1,7 +1,6 @@
 
 PRODUCT_COPY_FILES += \
 vendor/ev/prebuilt/gapps/system/app/Calendar.apk:system/app/Calendar.apk \
-vendor/ev/prebuilt/gapps/system/app/CalendarProvider.apk:system/app/CalendarProvider.apk \
 vendor/ev/prebuilt/gapps/system/app/ChromeBookmarksSyncAdapter.apk:system/app/ChromeBookmarksSyncAdapter.apk \
 vendor/ev/prebuilt/gapps/system/app/GenieWidget.apk:system/app/GenieWidget.apk \
 vendor/ev/prebuilt/gapps/system/app/Gmail.apk:system/app/Gmail.apk \
@@ -12,7 +11,6 @@ vendor/ev/prebuilt/gapps/system/app/GoogleLoginService.apk:system/app/GoogleLogi
 vendor/ev/prebuilt/gapps/system/app/GooglePackageVerifier.apk:system/app/GooglePackageVerifier.apk \
 vendor/ev/prebuilt/gapps/system/app/GooglePackageVerifierUpdater.apk:system/app/GooglePackageVerifierUpdater.apk \
 vendor/ev/prebuilt/gapps/system/app/GooglePartnerSetup.apk:system/app/GooglePartnerSetup.apk \
-vendor/ev/prebuilt/gapps/system/app/GoogleQuickSearchBox.apk:system/app/GoogleQuickSearchBox.apk \
 vendor/ev/prebuilt/gapps/system/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
 vendor/ev/prebuilt/gapps/system/app/GoogleTTS.apk:system/app/GoogleTTS.apk \
 vendor/ev/prebuilt/gapps/system/app/Maps.apk:system/app/Maps.apk \
@@ -21,6 +19,7 @@ vendor/ev/prebuilt/gapps/system/app/MediaUploader.apk:system/app/MediaUploader.a
 vendor/ev/prebuilt/gapps/system/app/Music2.apk:system/app/Music2.apk \
 vendor/ev/prebuilt/gapps/system/app/NetworkLocation.apk:system/app/NetworkLocation.apk \
 vendor/ev/prebuilt/gapps/system/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
+vendor/ev/prebuilt/gapps/system/app/QuickSearchBox.apk:system/app/QuickSearchBox.apk \
 vendor/ev/prebuilt/gapps/system/app/SetupWizard.apk:system/app/SetupWizard.apk \
 vendor/ev/prebuilt/gapps/system/app/Talk.apk:system/app/Talk.apk \
 vendor/ev/prebuilt/gapps/system/app/Vending.apk:system/app/Vending.apk \
