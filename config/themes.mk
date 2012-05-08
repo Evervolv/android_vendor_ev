060113ac452faaa5a81ef0cac224d0266dc0bcd1
andrew.boren <andrew.boren@gmail.com>
2012-05-07 22:11:58 -0700

    EV: Build theme packages.
    Change-Id: If99a3e95e3ee4745e5c1452e838d903a690f3d91

---
+++ config/themes.mk
@@ -0,0 +1,14 @@
+# T-Mobile theme engine
+PRODUCT_PACKAGES += \
+   ThemeManager \
+   ThemeChooser \
+   com.tmobile.themes
+
+PRODUCT_COPY_FILES += \
+    vendor/ev/prebuilt/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+
+
+# Theme packages to include
+
+PRODUCT_PACKAGES += \
+    Androidian-6-100
