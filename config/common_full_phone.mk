2e7b82446e7f70423c681999f21c681c953dd06b
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-06-05 00:25:20 -0500

    add apn list from cm
    Change-Id: Ic2f2a5e83ec00cdadd7aa12f1b44c2c4fc70eb76

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,6 +1,10 @@
 # Inherit common stuff
 $(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common.mk)
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    $(SRC_EVERVOLV_DIR)/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Selective SPN list for operator number who has the problem.
 PRODUCT_COPY_FILES += \
     $(SRC_EVERVOLV_DIR)/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
