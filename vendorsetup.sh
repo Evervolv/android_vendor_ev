ff1e6f5c2e627cc0ef5bb49df86886d396a4bc0c
andrew.boren <andrew.boren@gmail.com>
2012-07-14 21:13:22 -0700

    ev: add tenderloin (HP Touchpad) to lunch.
    Change-Id: I0cec7a1d26481dd0d2083dd88d0b945717d1905a

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,5 @@ add_lunch_combo ev_jewel-eng
 add_lunch_combo ev_passion-eng
 add_lunch_combo ev_shooter-eng
 add_lunch_combo ev_supersonic-eng
+add_lunch_combo ev_tenderloin-eng
 add_lunch_combo ev_toro-eng
