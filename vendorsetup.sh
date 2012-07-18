f5f57f6cad38f0aa853f0d205778f5638828d47f
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-07-18 17:51:09 -0500

    ev: add bravo/ruby to lunch
    Change-Id: Ib398a88c9f6976e87538c5b6a09a2a430b1721fc

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,9 @@
+add_lunch_combo ev_bravo-eng
 add_lunch_combo ev_grouper-eng
 add_lunch_combo ev_inc-eng
 add_lunch_combo ev_jewel-eng
 add_lunch_combo ev_passion-eng
+add_lunch_combo ev_ruby-eng
 add_lunch_combo ev_shooter-eng
 add_lunch_combo ev_supersonic-eng
 add_lunch_combo ev_tenderloin-eng
