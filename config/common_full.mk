5034119414d07de9c970c909f39adea7c47144ad
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-08-20 20:06:43 -0500

    evervolv: update dictionaries for jb
    pulled from cm: dictionaries can provide next word suggestions
    
    Change-Id: I1cf15ef6129c0ec174828179c947bfb253d6381c

--- config/common_full.mk
+++ config/common_full.mk
@@ -5,8 +5,12 @@ include frameworks/base/data/sounds/AllAudio.mk
 
 #include vendor/ev/config/themes.mk
 
+# English Dictionary
+PRODUCT_PACKAGE_OVERLAYS += vendor/ev/overlay/dictionaries/english
+
+# Provide non-english dictionaries if required
 ifeq ($(TARGET_USE_KEYBOARD), international)
-    PRODUCT_PACKAGE_OVERLAYS += vendor/ev/overlay/full
+    PRODUCT_PACKAGE_OVERLAYS += vendor/ev/overlay/dictionaries/international
 endif
 
 PRODUCT_PACKAGES += \
--- config/common_small.mk
+++ config/common_small.mk
@@ -1,3 +1,4 @@
 #Config used for small targets, with telephony or without.
 
-#PRODUCT_PACKAGE_OVERLAYS += vendor/ev/overlay/small
+# English Dictionary
+PRODUCT_PACKAGE_OVERLAYS += vendor/ev/overlay/dictionaries/english
