cdadff22a3a4d37b0c4cf3bc8c9d0b233636711f
Jmz <jmztaylor@gmail.com>
2012-09-23 10:09:19 -0700

    ThemeChooser bringup
    Change-Id: Ida90cb719e33523f100209ac85e2a6e6495e0cc9

--- config/themes.mk
+++ config/themes.mk
@@ -6,9 +6,3 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     vendor/ev/prebuilt/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
-
-
-# Theme packages to include
-
-PRODUCT_PACKAGES += \
-    Androidian-6-100
