5375db289df60f9a172120f31a307579a31fba20
Nick Reuter <nreuter85@gmail.com>
2013-04-17 13:56:32 -0500

    vendorsetup.sh: add support for sgh-t989
    Change-Id: Ide34e88596a9f5322341b6f05ef958c66683637c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,7 @@ add_lunch_combo ev_bravo-eng
 add_lunch_combo ev_d710-eng
 add_lunch_combo ev_glacier-eng
 add_lunch_combo ev_grouper-eng
+add_lunch_combo ev_hercules-eng
 add_lunch_combo ev_inc-eng
 add_lunch_combo ev_jewel-eng
 add_lunch_combo ev_kingdom-eng
