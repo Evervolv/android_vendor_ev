298b4a361143368412ea7bbd08c0b8b888edbfb9
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-05-25 19:15:04 -0500

    envsetup: merge breakfast into lunch
    the lazy folk can now type "lunch mako"
    breakfast will be phased out on next release, but nightly script still
    uses it for now
    
    Change-Id: Iaef9c550d124f41dcf623671aed2e7cbe17c783d

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -20,32 +20,7 @@ function find_deps() {
 
 function breakfast()
 {
-    target=$1
-    EV_DEVICES_ONLY="true"
-    unset LUNCH_MENU_CHOICES
-    add_lunch_combo full-eng
-    for f in `/bin/ls vendor/ev/vendorsetup.sh 2> /dev/null`
-        do
-            echo "including $f"
-            . $f
-        done
-    unset f
-
-    if [ $# -eq 0 ]; then
-        # No arguments, so let's have the full menu
-        echo "Nothing to eat for breakfast?"
-        lunch
-    else
-        echo "z$target" | grep -q "-"
-        if [ $? -eq 0 ]; then
-            # A buildtype was specified, assume a full device name
-            lunch $target
-        else
-            # This is probably just the EV model name
-            lunch ev_$target-eng
-        fi
-    fi
-    return $?
+    lunch $@
 }
 
 function cleantree () {
