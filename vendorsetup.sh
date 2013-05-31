6127a6861df9509055a06d0619d6fe867793642c
Dorian Snyder <dastin1015@gmail.com>
2013-05-30 18:31:22 -0700

    ev: change m7wls to m7spr for carrier variant split
    Change-Id: Idbbbb5f4009fe0d2ebd26cde57da10edeb86374f

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,7 +6,7 @@ add_lunch_combo ev_hercules-eng
 add_lunch_combo ev_inc-eng
 add_lunch_combo ev_jewel-eng
 add_lunch_combo ev_lexikon-eng
-add_lunch_combo ev_m7wls-eng
+add_lunch_combo ev_m7spr-eng
 add_lunch_combo ev_mako-eng
 add_lunch_combo ev_passion-eng
 add_lunch_combo ev_pyramid-eng
