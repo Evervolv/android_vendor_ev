f9eb761e00e896f61c54a5bde163cadaeb3db1c0
Nick Reuter <nreuter85@gmail.com>
2014-06-16 23:29:51 -0500

    ev: add permissions for CM Theme Engine
    Change-Id: Id4d16a7e39e1828ffb10fac123ff36a2a5528344

--- config/themes.mk
+++ config/themes.mk
@@ -1,8 +1,8 @@
 # T-Mobile theme engine
 PRODUCT_PACKAGES += \
-   ThemeManager \
    ThemeChooser \
-   com.tmobile.themes
+   ThemeProvider
 
 PRODUCT_COPY_FILES += \
-    $(SRC_EVERVOLV_DIR)/prebuilt/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+    $(SRC_EVERVOLV_DIR)/prebuilt/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    $(SRC_EVERVOLV_DIR)/prebuilt/common/etc/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml \
