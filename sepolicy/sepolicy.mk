4db26c1ea4c8526de7aceaabd7d3b49b4e0f29e7
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-09 17:20:54 +0000

    selinux: squash cm updates to sepolicy
     - Updates for CM12 49a30e7d179d4e529c0f79b637a4f69e43430f82
     - selinux: Add rules for the audit daemon 15df17f9acf1ca652d9495218846212e682a64d5
    
    Change-Id: Ia4833bbd8aefc9bb192b507242c8b5a22113e627

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,19 +1,20 @@
 #
 # This policy configuration will be used by all products that
-# use SELinux
+# inherit from CM
 #
 
 BOARD_SEPOLICY_DIRS += \
-    vendor/ev/sepolicy
+    vendor/cm/sepolicy
 
 BOARD_SEPOLICY_UNION += \
     file.te \
     file_contexts \
-    fs_use \
     genfs_contexts \
     seapp_contexts \
+    auditd.te \
     installd.te \
     netd.te \
+    system.te \
     ueventd.te \
     vold.te \
     mac_permissions.xml
