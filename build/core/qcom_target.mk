f448565d61ad1cb7ce8d29381373a8db3e09b3de
Steve Kondik <steve@cyngn.com>
2014-12-24 21:58:01 -0800

    build: Set QCOM global cflags for secondary arch
    Change-Id: I637d94f9b0ddee2c8d6057de44355e482163dd36

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -35,6 +35,12 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
     CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
 
+    # Multiarch needs these too..
+    2ND_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    2ND_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+    2ND_CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    2ND_CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+
     ifneq ($(filter msm8084,$(TARGET_BOARD_PLATFORM)),)
         #This is for 8084 based platforms
         QCOM_AUDIO_VARIANT := audio-caf/msm8084
