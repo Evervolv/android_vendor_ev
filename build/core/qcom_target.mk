559040e38722d26efa653a83f06a1df8977c314f
Anthony King <anthonydking@slimroms.net>
2015-09-02 11:01:31 -0700

    build: Allow QC platforms to share/customize HAL variants
    * Allow devices to override HAL variant using QCOM_HARDWARE_VARIANT
    * 8909 and 8916 share HALs by default
    * 8610, 8226, 8974 share HALs by default
    * 8992, 8994 share HALs by default
    
    Change-Id: Idfbdc405a35c5ba518fc71c8b691fc177ddf9068

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -42,18 +42,24 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     2ND_CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
 
     ifneq ($(filter msm8084,$(TARGET_BOARD_PLATFORM)),)
-        #This is for 8084 based platforms
         QCOM_AUDIO_VARIANT := audio-caf/msm8084
         QCOM_DISPLAY_VARIANT := display-caf/msm8084
         QCOM_MEDIA_VARIANT := media-caf/msm8084
     else ifneq ($(filter msm8610 msm8226 msm8974,$(TARGET_BOARD_PLATFORM)),)
-        #This is for 8974 based (and B-family) platforms
         QCOM_AUDIO_VARIANT := audio-caf/msm8974
         QCOM_DISPLAY_VARIANT := display-caf/msm8974
         QCOM_MEDIA_VARIANT := media-caf/msm8974
+    else ifneq ($(filter msm8909 msm8916,$(TARGET_BOARD_PLATFORM)),)
+        QCOM_AUDIO_VARIANT := audio-caf/msm8916
+        QCOM_DISPLAY_VARIANT := display-caf/msm8916
+        QCOM_MEDIA_VARIANT := media-caf/msm8916
+    else ifneq ($(filter msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+        QCOM_AUDIO_VARIANT := audio-caf/msm8994
+        QCOM_DISPLAY_VARIANT := display-caf/msm8994
+        QCOM_MEDIA_VARIANT := media-caf/msm8994
     else
         QCOM_AUDIO_VARIANT := audio-caf/msm8960
-        # Enables legacy repos to be handled
+        # Handle legacy repos
         ifeq ($(BOARD_USES_LEGACY_QCOM_DISPLAY),true)
             QCOM_DISPLAY_VARIANT := display-legacy
             QCOM_MEDIA_VARIANT := media-legacy
