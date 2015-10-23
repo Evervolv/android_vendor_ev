3dd4fa557410ed217c1cad2b69bc775c4e97f448
Nick Reuter <nreuter85@gmail.com>
2015-10-22 20:56:52 -0500

    core: resync with cm
     remove legacy hal calls, they can be done per device,
     enabled 7x30/8660/8960 to share the same hals
    
    Change-Id: If5ded3773d3b178d76c15c9120d90862128ba874
    
    core: fix qcom_target for display-legacy usage
    
    Change-Id: Ib129558cf2ab06d90ab3ecf6d000cd86c65dd029

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -7,18 +7,26 @@ endef
 define ril-set-path-variant
 $(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/$(1))
 endef
+define gps-hal-set-path-variant
+$(call project-set-path-variant,gps-hal,TARGET_GPS_HAL_PATH,$(1))
+endef
+define loc-api-set-path-variant
+$(call project-set-path-variant,loc-api,TARGET_LOC_API_PATH,$(1))
+endef
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
     qcom_flags := -DQCOM_HARDWARE
-
     ifneq ($(BOARD_USES_LEGACY_QCOM_DISPLAY),true)
-        TARGET_USES_QCOM_BSP := true
         qcom_flags += -DQCOM_BSP
         qcom_flags += -DQTI_BSP
-    endif
 
+        TARGET_USES_QCOM_BSP := true
+    endif
     TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+    # Tell HALs that we're compiling an AOSP build with an in-line kernel
+    TARGET_COMPILE_WITH_MSM_KERNEL := true
 
     # Enable DirectTrack for legacy targets
     ifneq ($(filter msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),)
@@ -42,58 +50,55 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     2ND_CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
     2ND_CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
 
-    ifneq ($(filter msm8084,$(TARGET_BOARD_PLATFORM)),)
-        QCOM_AUDIO_VARIANT := audio-caf/msm8084
-        QCOM_DISPLAY_VARIANT := display-caf/msm8084
-        QCOM_MEDIA_VARIANT := media-caf/msm8084
-    else ifneq ($(filter msm8610 msm8226 msm8974,$(TARGET_BOARD_PLATFORM)),)
-        QCOM_AUDIO_VARIANT := audio-caf/msm8974
-        QCOM_DISPLAY_VARIANT := display-caf/msm8974
-        QCOM_MEDIA_VARIANT := media-caf/msm8974
-    else ifneq ($(filter msm8909 msm8916,$(TARGET_BOARD_PLATFORM)),)
-        QCOM_AUDIO_VARIANT := audio-caf/msm8916
-        QCOM_DISPLAY_VARIANT := display-caf/msm8916
-        QCOM_MEDIA_VARIANT := media-caf/msm8916
-    else ifneq ($(filter msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
-        QCOM_AUDIO_VARIANT := audio-caf/msm8994
-        QCOM_DISPLAY_VARIANT := display-caf/msm8994
-        QCOM_MEDIA_VARIANT := media-caf/msm8994
-    else
-        QCOM_AUDIO_VARIANT := audio-caf/msm8960
-        # Handle legacy repos
-        ifeq ($(BOARD_USES_LEGACY_QCOM_DISPLAY),true)
-            QCOM_DISPLAY_VARIANT := display-legacy
-            QCOM_MEDIA_VARIANT := media-legacy
+    ifeq ($(QCOM_HARDWARE_VARIANT),)
+        ifneq ($(filter msm8610 msm8226 msm8974,$(TARGET_BOARD_PLATFORM)),)
+            QCOM_HARDWARE_VARIANT := msm8974
         else
-            QCOM_DISPLAY_VARIANT := display-caf/msm8960
-            QCOM_MEDIA_VARIANT := media-caf/msm8960
+        ifneq ($(filter msm8909 msm8916,$(TARGET_BOARD_PLATFORM)),)
+            QCOM_HARDWARE_VARIANT := msm8916
+        else
+        ifneq ($(filter msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+            QCOM_HARDWARE_VARIANT := msm8994
+        else
+        ifneq ($(filter msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),)
+            QCOM_HARDWARE_VARIANT := msm8960
+        else
+            QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
+        endif
+        endif
+        endif
         endif
     endif
 
-else
-    # QSD8K doesn't use QCOM_HARDWARE flag
-    ifneq ($(filter qsd8k,$(TARGET_BOARD_PLATFORM)),)
-        QCOM_AUDIO_VARIANT := audio-caf/msm8960
-    else
-        QCOM_AUDIO_VARIANT := audio
-    endif
-    ifeq ($(BOARD_USES_LEGACY_QCOM_DISPLAY),true)
-        QCOM_DISPLAY_VARIANT := display-legacy
-        QCOM_MEDIA_VARIANT := media-legacy
-    else
-        QCOM_DISPLAY_VARIANT := display/$(TARGET_BOARD_PLATFORM)
-        QCOM_MEDIA_VARIANT := media
-    endif
-endif
-
-$(call project-set-path,qcom-audio,hardware/qcom/$(QCOM_AUDIO_VARIANT))
-$(call project-set-path,qcom-display,hardware/qcom/$(QCOM_DISPLAY_VARIANT))
-$(call project-set-path,qcom-media,hardware/qcom/$(QCOM_MEDIA_VARIANT))
+$(call project-set-path,qcom-audio,hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT))
 ifeq ($(USE_DEVICE_SPECIFIC_CAMERA),true)
 $(call project-set-path,qcom-camera,$(TARGET_DEVICE_DIR)/camera)
 else
 $(call qcom-set-path-variant,CAMERA,camera)
 endif
+ifeq ($(BOARD_USES_LEGACY_QCOM_DISPLAY),true)
+$(call project-set-path,qcom-display,hardware/qcom/display-legacy)
+else
+$(call project-set-path,qcom-display,hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT))
+endif
 $(call qcom-set-path-variant,GPS,gps)
+ifeq ($(BOARD_USES_LEGACY_QCOM_DISPLAY),true)
+$(call project-set-path,qcom-media,hardware/qcom/media-legacy)
+else
+$(call project-set-path,qcom-media,hardware/qcom/media-caf/$(QCOM_HARDWARE_VARIANT))
+endif
 $(call qcom-set-path-variant,SENSORS,sensors)
 $(call ril-set-path-variant,ril)
+$(call loc-api-set-path-variant,vendor/qcom/opensource/location)
+$(call gps-hal-set-path-variant,hardware/qcom/gps)
+else
+$(call project-set-path,qcom-audio,hardware/qcom/audio)
+$(call qcom-set-path-variant,CAMERA,camera)
+$(call project-set-path,qcom-display,hardware/qcom/display/$(TARGET_BOARD_PLATFORM))
+$(call qcom-set-path-variant,GPS,gps)
+$(call project-set-path,qcom-media,hardware/qcom/media)
+$(call qcom-set-path-variant,SENSORS,sensors)
+$(call ril-set-path-variant,ril)
+$(call loc-api-set-path-variant,vendor/qcom/opensource/location)
+$(call gps-hal-set-path-variant,hardware/qcom/gps)
+endif
