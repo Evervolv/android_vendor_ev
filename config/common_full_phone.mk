c83da1ad68186f113926b4a64cd7807c6b4efb1a
razorloves <razorloves@gmail.com>
2016-02-18 23:47:47 -0600

    Revert "cm: add selective based spn (2/2)"
    Appears to be unused in CM13.
    
    This reverts commit 2cfe6b260bf743cbf3091095807b373037581181.
    
    Change-Id: Ia9beee776fd86f516ed9834f92530355a189f326

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -5,10 +5,6 @@ $(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common.mk)
 PRODUCT_COPY_FILES += \
     $(SRC_EVERVOLV_DIR)/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
-# Selective SPN list for operator number who has the problem.
-PRODUCT_COPY_FILES += \
-    $(SRC_EVERVOLV_DIR)/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
-
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk
