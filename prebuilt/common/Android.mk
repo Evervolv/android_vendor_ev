ff3bfcc1dad81873d71d1c2d195a8b781ac17aa1
Nick Reuter <nreuter85@gmail.com>
2016-06-07 22:32:40 -0500

    ev: cleanup makefiles
    Change-Id: I6cedd3d518b55b3ecb2921ac565dca1fe98ce5ea

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -1,10 +1,24 @@
 LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := Chromium
+LOCAL_SRC_FILES := apk/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+LOCAL_OVERRIDES_PACKAGES := Browser
+LOCAL_MULTILIB := both
+
+include $(BUILD_PREBUILT)
+
 # a wrapper for curl which provides wget syntax, for compatibility
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := wget
-LOCAL_SRC_FILES := bin/wget
+LOCAL_SRC_FILES := bin/$(LOCAL_MODULE)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+
 include $(BUILD_PREBUILT)
