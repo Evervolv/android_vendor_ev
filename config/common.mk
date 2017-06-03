548041166160b9fc2d5af02485607197224dabaa
Nick Reuter <nreuter85@gmail.com>
2017-06-03 02:33:29 -0500

    config: cleanup default props
    Change-Id: I7317d9efd95bffccd467d10b1103e022a24b3eed

--- config/common.mk
+++ config/common.mk
@@ -1,23 +1,28 @@
 # Evervolv
 PRODUCT_BRAND ?= evervolv
 
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
 # Default propety overrides
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
+    dalvik.vm.debug.alloc=0 \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false \
-    ro.build.selinux=1 \
-    persist.sys.strictmode.disable=true \
-    ro.substratum.verified=true
+    ro.build.selinux=1
 
 # Disable UTC date
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_UTC_DATE=0
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += $(SRC_EVERVOLV_DIR)/overlay/common
