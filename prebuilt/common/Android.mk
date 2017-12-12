d58a852f5002850f2ff2522f40a13fb941f5662c
Nick Reuter <nreuter85@gmail.com>
2017-12-11 22:21:40 -0600

    vendor/ev: add android.hidl.base@1.0 stub
    Change-Id: I0593394eec71d47c5056f25be8a161f4187355e3

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -26,3 +26,10 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := etc/init.local.rc
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hidl.base@1.0
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES :=
+LOCAL_SHARED_LIBRARIES := libhidltransport
+include $(BUILD_SHARED_LIBRARY)
