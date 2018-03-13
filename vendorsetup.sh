deb659ec6a1a48386a054ead832b77b2bb952ab5
Alex Cruz <du.alexcruz@gmail.com>
2018-03-13 03:02:11 -0400

    ev: Add all build types to the lunch menu
    Change-Id: I7ab757f04d9e101306a21cd0f4bd88754e086108

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,5 +9,7 @@ supported_devices=(
 )
 
 for device in ${supported_devices[@]}; do
+    add_lunch_combo ev_${device}-eng
+    add_lunch_combo ev_${device}-user
     add_lunch_combo ev_${device}-userdebug
 done
