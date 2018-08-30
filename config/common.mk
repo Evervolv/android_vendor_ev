7dbd78933c94e96d810ea52a109bd4c91c347944
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-08-30 14:41:32 +0200

    Add rules for non platform apps that use hidden APIs
    Change-Id: I483f00cc0735f47665c5cb61ed0a187fb4e7d655

--- config/common.mk
+++ config/common.mk
@@ -37,7 +37,8 @@ PRODUCT_COPY_FILES += \
 
 # Permissions for our apps
 PRODUCT_COPY_FILES += \
-    $(SRC_EVERVOLV_DIR)/prebuilt/common/etc/permissions/privapp-permissions.xml:system/etc/permissions/privapp-permissions-evervolv.xml
+    $(SRC_EVERVOLV_DIR)/prebuilt/common/etc/permissions/privapp-permissions.xml:system/etc/permissions/privapp-permissions-evervolv.xml \
+    $(SRC_EVERVOLV_DIR)/prebuilt/common/etc/permissions/hiddenapi-package-whitelist.xml:system/etc/permissions/evervolv-hiddenapi-package-whitelist.xml
 
 # Overlays
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(SRC_EVERVOLV_DIR)/overlay
