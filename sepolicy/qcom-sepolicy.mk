6c411ce3a235b986948beb59a6868e5a49a6c258
dianlujitao <dianlujitao@lineageos.org>
2019-02-14 13:28:42 +0800

    sepolicy: Dynamically build touch hal policy into system/vendor
     * Introduce a new board flag TARGET_USES_PREBUILT_VENDOR_SEPOLICY and
       a sepolicy variant: dynamic
     * When TARGET_USES_PREBUILT_VENDOR_SEPOLICY=true, dynamic act as
       private policy, and vendor policy is excluded in order to avoid
       conflicts (it's not integrated to final builds anyway). When the flag
       is not set, dynamic acts as vendor policy to survive from system
       image change i.e. GSI installation.
    
    Change-Id: I8bfd078d6064616c88e2c58a9fa3aa045dddf303
    
    sepolicy: Move touch hal policy to dynamic
    
    Change-Id: I7beb51197b6049133b4385ef344e21fbf2de1ea0

--- sepolicy/qcom-sepolicy.mk
+++ sepolicy/qcom-sepolicy.mk
@@ -3,9 +3,17 @@
 # that inherit from Lineage
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     $(SRC_EVERVOLV_DIR)/sepolicy/qcom/private
 
+ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
     $(SRC_EVERVOLV_DIR)/sepolicy/qcom/common \
     $(SRC_EVERVOLV_DIR)/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+endif
