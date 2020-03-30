044e4ab109a47fc2fe60a5fbb66863f5d6a1188b
theimpulson <aayushgupta219@gmail.com>
2020-03-30 10:12:45 +0000

    lineage: Introduce a new flag to exclude fuseblk sepolicy
    Some QCOM and Almost all MTK devices ship fuseblk sepolicy on vendor partition.
    When using a prebuilt vendor.img, this causes conflicts with our fuseblk sepolicy,
    which prevents the device from booting.
    To remedy this, allow excluding fuseblk sepolicy at build time.
    
    Signed-off-by: theimpulson <aayushgupta219@gmail.com>
    Change-Id: I938a14ada3bb9b6ffa20bb27c15adae3e493b98c

--- sepolicy/common/sepolicy.mk
+++ sepolicy/common/sepolicy.mk
@@ -4,6 +4,12 @@ TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
 endif
 endif
 
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+ifeq ($(TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR),true)
+BOARD_SEPOLICY_M4DEFS += board_excludes_fuseblk_sepolicy=true
+endif
+endif
+
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     $(SRC_EVERVOLV_DIR)/sepolicy/common/public
 
