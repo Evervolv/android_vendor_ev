630babf2ff908026aec4b87d1155781b4dcc579b
surblazer <matvej2002@gmail.com>
2020-11-29 00:32:21 +0300

    qcom: add msm8960 and apq8084 into upstream list
    msm8960 and apq8084 also needs this upstream for its sepolicy to avoid breaking the build
    
    Change-Id: I2dd836278cc6f899021679bdaddf339bd29141b0

--- sepolicy/qcom/sepolicy.mk
+++ sepolicy/qcom/sepolicy.mk
@@ -16,7 +16,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     $(SRC_EVERVOLV_DIR)/sepolicy/qcom/vendor
 endif
 
-ifeq (,$(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994 msm8937 msm8953 msm8996 sdm660 sdm710 sdm845, $(TARGET_BOARD_PLATFORM)))
+ifeq (,$(filter msm8960 msm8226 msm8610 msm8974 apq8084 msm8909 msm8916 msm8952 msm8992 msm8994 msm8937 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845, $(TARGET_BOARD_PLATFORM)))
 BOARD_SEPOLICY_M4DEFS += \
     qdisplay_service=vendor_qdisplay_service \
     sysfs_graphics=vendor_sysfs_graphics \
