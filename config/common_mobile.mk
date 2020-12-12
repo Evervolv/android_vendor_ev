4ccb84cf30258dcbe955028207591666718ae792
Dyneteve <dyneteve@hentaios.com>
2020-12-13 00:57:11 +0900

    config: Build the textclassifier models
    Change-Id: I9d7d582ddc427998e68e668cc0375958325d8052

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -72,6 +72,13 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     QuickAccessWallet
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    libtextclassifier_annotator_en_model \
+    libtextclassifier_annotator_universal_model \
+    libtextclassifier_actions_suggestions_universal_model \
+    libtextclassifier_lang_id_model
+
 # Themes
 PRODUCT_PACKAGES += \
     LineageBlackTheme \
