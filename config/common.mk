fedef4bff7a4fc60f91bcf5c62f0b0f286a8b302
Nick Reuter <nreuter85@gmail.com>
2021-07-07 19:07:17 -0500

    ev: update redfin fingerprint to RQ3A.210705.001
    Change-Id: Ie2fec497c779f0ede656b3798352aa587e754944

--- config/common.mk
+++ config/common.mk
@@ -45,7 +45,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 # Build information
-PRODUCT_OVERRIDE_FINGERPRINT ?= google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+PRODUCT_OVERRIDE_FINGERPRINT ?= google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
 
 # Command line
 PRODUCT_PACKAGES += \
