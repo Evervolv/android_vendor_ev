164accd2ed2edaf9b9b7bc3065e5199174a06605
Alexander Koskovich <zvnexus@outlook.com>
2021-08-01 08:54:51 -0700

    lineage: Add framework compatibility matrix for Lineage HALs.
     * Cover all possible Lineage HALs here instead of covering
       them in the device specific manifests.
    
    Change-Id: Id7f7d6eb1f14190a061537cebfabd339af9b1620

---
+++ config/mainline.mk
@@ -0,0 +1,4 @@
+# Compatibility
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    hardware/evervolv/interfaces/compatibility_matrices/compatibility_matrix.evervolv.xml \
+    hardware/lineage/interfaces/compatibility_matrices/compatibility_matrix.lineage.xml
