c9fd127af2b91f10cd43bb397c62d2ee2a807979
Soo-Hwan Na <69458352+roynatech2544@users.noreply.github.com>
2021-12-08 08:23:34 +0900

    ev: update build fingerprint
    Change-Id: I84d414c2f64a26e393cbff14590d0b64d82fcea6

--- config/common.mk
+++ config/common.mk
@@ -58,7 +58,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 # Build information
-PRODUCT_OVERRIDE_FINGERPRINT ?= google/redfin/redfin:12/SP1A.210812.016.A1/7796139:user/release-keys
+PRODUCT_OVERRIDE_FINGERPRINT ?= google/redfin/redfin:12/SQ1A.211205.008/7888514:user/release-keys
 
 # Command line
 PRODUCT_PACKAGES += \
