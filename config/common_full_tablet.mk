d9990e1b3172591b6d3fb6ac409c4e370a87df54
Bruno Martins <bgcngm@gmail.com>
2022-03-11 12:10:45 +0000

    config: Create makefiles for tablets with telephony support
    So far, tablets with telephony have been inheriting from
    common_full_phone.mk, but that's not really proper because
    such makefile now enables one-handed mode support
    (setting prop ro.support_one_handed_mode to true).
    
    Fix this by creating new makefiles that can be used also
    to include more tablet-specific configs moving forward.
    
    Change-Id: I90c22badb17911ef5e873299d986204718300e6c

--- config/common_full_tablet.mk
+++ config/common_full_tablet.mk
@@ -1,7 +1,3 @@
 # Inherit common stuff
 $(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common.mk)
 $(call inherit-product, $(SRC_EVERVOLV_DIR)/config/telephony.mk)
-
-# Enable support of one-handed mode
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.support_one_handed_mode=true
---
+++ config/telephony.mk
@@ -0,0 +1,13 @@
+# Telephony packages
+PRODUCT_PACKAGES += \
+    apns-conf.xml \
+    sensitive_pn.xml \
+    messaging \
+    Stk \
+    CellBroadcastReceiver
+
+# Legacy support
+ifeq ($(TARGET_USES_OLD_MNC_FORMAT), true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.use_old_mnc_mcc_format=true
+endif
