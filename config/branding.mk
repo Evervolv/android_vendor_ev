7a727293bdbbf7c9e3e75bfd172d8286443fe556
Nick Reuter <nreuter85@gmail.com>
2022-05-09 22:12:53 -0500

    ev: update branding
     - use ?= where possible
     - allow hh:mm time to be removed from the filename
    
    Change-Id: I922284d0c5872d1b68e7b603cb3655a0210df6e0

--- config/branding.mk
+++ config/branding.mk
@@ -1,4 +1,5 @@
 # Boot animation
+BOOT_ANIMATION_SIZE ?= 1080p
 ifneq ($(filter 720p 1080p 1440p hvga qhd wvga xga,$(BOOT_ANIMATION_SIZE)),)
 PRODUCT_COPY_FILES += \
     $(SRC_EVERVOLV_DIR)/prebuilt/bootanimation/$(BOOT_ANIMATION_SIZE).zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
@@ -45,9 +46,11 @@ endif
 
 ifneq ($(filter nightly testing release,$(PRODUCT_BUILD)),)
 EV_BUILD_TYPE := $(PRODUCT_BUILD)
-else
-EV_BUILD_TYPE := userbuild
 endif
+EV_BUILD_TYPE ?= userbuild
 
-EV_PACKAGE_NAME := $(TARGET_PRODUCT)-$(EV_VERSION)-$(EV_BUILD_TYPE)-$(shell date +%Y.%m.%d)-$(shell date -u +%H%M)
+ifeq ($(SKIP_VERBOSE_DATE),true)
+EV_PACKAGE_NAME := $(TARGET_PRODUCT)-$(EV_VERSION)-$(EV_BUILD_TYPE)-$(shell date +%Y.%m.%d)
+endif
+EV_PACKAGE_NAME ?= $(TARGET_PRODUCT)-$(EV_VERSION)-$(EV_BUILD_TYPE)-$(shell date +%Y.%m.%d)-$(shell date -u +%H%M)
 TARGET_OTA_PACKAGE_NAME := $(shell echo ${EV_PACKAGE_NAME} | tr [:upper:] [:lower:])
