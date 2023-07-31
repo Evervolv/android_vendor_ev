ae75aae21dbd9b9052b1a16900a469c34bbc9ca9
Nick Reuter <nreuter85@gmail.com>
2023-07-30 23:38:19 -0500

    ev: move bootanimation to prebuilt directory
    Change-Id: I0874ced4b965aabef2c11851da17126dca46ab8a

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -29,3 +29,53 @@ LOCAL_PREBUILT_MODULE_FILE := $(FINAL_APNS_FILE)
 LOCAL_PRODUCT_MODULE := true
 
 include $(BUILD_PREBUILT)
+
+TARGET_GENERATED_BOOTANIMATION := $(TARGET_OUT_INTERMEDIATES)/BOOTANIMATION/bootanimation.zip
+$(TARGET_GENERATED_BOOTANIMATION): INTERMEDIATES := $(TARGET_OUT_INTERMEDIATES)/BOOTANIMATION
+$(TARGET_GENERATED_BOOTANIMATION): $(SOONG_ZIP)
+	@echo "Building bootanimation.zip"
+	@rm -rf $(dir $@)
+	@mkdir -p $(dir $@)
+	$(hide) tar xfp $(SRC_EVERVOLV_DIR)/prebuilt/bootanimation/bootanimation.tar -C $(INTERMEDIATES)
+	$(hide) if [ $(TARGET_SCREEN_HEIGHT) -lt $(TARGET_SCREEN_WIDTH) ]; then \
+	    IMAGEWIDTH=$(TARGET_SCREEN_HEIGHT); \
+	else \
+	    IMAGEWIDTH=$(TARGET_SCREEN_WIDTH); \
+	fi; \
+	IMAGESCALEWIDTH=$$IMAGEWIDTH; \
+	IMAGESCALEHEIGHT=$$(expr $$IMAGESCALEWIDTH / 3); \
+	if [ "$(TARGET_BOOTANIMATION_HALF_RES)" = "true" ]; then \
+	    IMAGEWIDTH="$$(expr "$$IMAGEWIDTH" / 2)"; \
+	fi; \
+	IMAGEHEIGHT=$$(expr $$IMAGEWIDTH / 3); \
+	RESOLUTION="$$IMAGEWIDTH"x"$$IMAGEHEIGHT"; \
+	for part_cnt in 0 1; do \
+	    mkdir -p $(INTERMEDIATES)/part$$part_cnt; \
+	done; \
+	prebuilts/evervolv-tools/${HOST_OS}-x86/bin/mogrify -resize $$RESOLUTION -colors 250 $(INTERMEDIATES)/*/*.png; \
+	echo "$$IMAGESCALEWIDTH $$IMAGESCALEHEIGHT 15" > $(INTERMEDIATES)/desc.txt; \
+	cat $(SRC_EVERVOLV_DIR)/prebuilt/bootanimation/desc.txt >> $(INTERMEDIATES)/desc.txt
+	$(hide) $(SOONG_ZIP) -L 0 -o $(TARGET_GENERATED_BOOTANIMATION) -C $(INTERMEDIATES) -D $(INTERMEDIATES)
+
+ifeq ($(TARGET_BOOTANIMATION),)
+    TARGET_BOOTANIMATION := $(TARGET_GENERATED_BOOTANIMATION)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bootanimation.zip
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/media
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): $(TARGET_BOOTANIMATION)
+	@cp $(TARGET_BOOTANIMATION) $@
+
+include $(CLEAR_VARS)
+
+BOOTANIMATION_SYMLINK := $(TARGET_OUT_PRODUCT)/media/bootanimation-dark.zip
+$(BOOTANIMATION_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf bootanimation.zip $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(BOOTANIMATION_SYMLINK)
