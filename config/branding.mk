04c925fa5ed8b815ba69c788b55579e515c42bb5
Nick Reuter <nreuter85@gmail.com>
2023-11-23 00:09:44 -0600

    ev: remove legacy boot animation helpers
    Change-Id: Iaba93e7de049b0fda8aef53f9c0e4cd3c375b6bc

--- config/branding.mk
+++ config/branding.mk
@@ -1,16 +1,3 @@
-# Boot animation
-BOOT_ANIMATION_SIZE ?= 1080p
-ifeq ($(BOOT_ANIMATION_SIZE),1440p)
-TARGET_SCREEN_HEIGHT := 3120
-TARGET_SCREEN_WIDTH := 1440
-else ifeq ($(BOOT_ANIMATION_SIZE),1080p)
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-else ifeq ($(BOOT_ANIMATION_SIZE),720p)
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-endif
-
 # AOSP has no support of loading framework resources from /system_ext
 # so the SDK has to stay in /system for now
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
