0861ecb7f2396bec96a437054e4683e98628f207
Nick Reuter <nreuter85@gmail.com>
2024-01-21 12:15:56 -0600

    Revert "common_tv: move lineage/atv inclusion to devices"
    This reverts commit 7f8b76f792805ff19d082276baf36d29fab7ffe1.

--- config/common_tv.mk
+++ config/common_tv.mk
@@ -1,6 +1,9 @@
 # Inherit common stuff
 $(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common.mk)
 
+# Inherit Lineage atv device tree
+$(call inherit-product-if-exists, device/lineage/atv/lineage_atv.mk)
+
 # AOSP packages
 PRODUCT_PACKAGES += \
     LeanbackIME
