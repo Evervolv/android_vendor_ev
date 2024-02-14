742c0527e24f6589692ebae2c69e17074af0c084
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-02-14 23:44:31 +0100

    bootanimation: Use 256 colors
    And remove unused mkdir rules
    
    Change-Id: Ic6df2c46938c7ecc0bd8a80712b38b57ff15d7c9

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -49,10 +49,7 @@ $(TARGET_GENERATED_BOOTANIMATION): $(SOONG_ZIP)
 	fi; \
 	IMAGEHEIGHT=$$(expr $$IMAGEWIDTH / 3); \
 	RESOLUTION="$$IMAGEWIDTH"x"$$IMAGEHEIGHT"; \
-	for part_cnt in 0 1; do \
-	    mkdir -p $(INTERMEDIATES)/part$$part_cnt; \
-	done; \
-	prebuilts/evervolv-tools/${HOST_OS}-x86/bin/mogrify -resize $$RESOLUTION -colors 250 $(INTERMEDIATES)/*/*.png; \
+	prebuilts/evervolv-tools/${HOST_OS}-x86/bin/mogrify -resize $$RESOLUTION -colors 256 $(INTERMEDIATES)/*/*.png; \
 	echo "$$IMAGESCALEWIDTH $$IMAGESCALEHEIGHT 15" > $(INTERMEDIATES)/desc.txt; \
 	cat $(SRC_EVERVOLV_DIR)/prebuilt/bootanimation/desc.txt >> $(INTERMEDIATES)/desc.txt
 	$(hide) $(SOONG_ZIP) -L 0 -o $@ -C $(INTERMEDIATES) -D $(INTERMEDIATES)
