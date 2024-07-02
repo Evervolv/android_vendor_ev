a50cd1e75a67dc196790fe39b39ce8feb9247584
Nick Reuter <nreuter85@gmail.com>
2024-07-01 19:33:20 -0500

    common: use include instead of inherit for extra makefile
    Change-Id: I2c5e704296ac85d0f770618d20e5438b89f76b72

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,5 @@
 # Allow vendor/extra to override any property by setting it first
-$(call inherit-product-if-exists, vendor/extra/product.mk)
+-include vendor/extra/product.mk
 
 # ADB
 PRODUCT_PACKAGES += \
