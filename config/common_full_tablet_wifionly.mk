7f00dea9832a457811e228552bf6446c6906231f
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-21 21:04:37 +0200

    config: tablet: Deduplicate full tablet configuration
    Change-Id: I12cc09d4fca28d063cea76301de287526bfec570

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,10 +1,7 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
-
 # Inherit mobile full common stuff
 $(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common_mobile_full.mk)
 
-# Settings
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.settings.large_screen_opt.enabled=true
+# Inherit full tablet common stuff
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/full_tablet.mk)
 
 $(call inherit-product, $(SRC_EVERVOLV_DIR)/config/wifionly.mk)
