befcac5a216328d8d293a62626732080276222ec
Nick Reuter <nreuter85@gmail.com>
2024-09-28 10:30:02 -0500

    Revert "Revert "Revert "common_tv: move lineage/atv inclusion to devices"""
    This reverts commit 5f154618ec9862d673807d0199d5c9b8ee3c9c96.

--- config/common_tv.mk
+++ config/common_tv.mk
@@ -4,6 +4,9 @@ $(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common.mk)
 # Include AOSP audio files
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioTv.mk)
 
+# Inherit Lineage atv device tree
+$(call inherit-product-if-exists, device/lineage/atv/lineage_atv.mk)
+
 # AOSP packages
 PRODUCT_PACKAGES += \
     LeanbackIME
