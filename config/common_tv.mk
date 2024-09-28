389ee62c5518a83f59860d8dac2a5bbdc7624191
Nick Reuter <nreuter85@gmail.com>
2024-09-28 10:30:00 -0500

    Reapply "atv: Build LeanbackIME"
    This reverts commit 6ac1b9492aa3f84c6c64f22eea3dd3ba98966b7b.

--- config/common_tv.mk
+++ config/common_tv.mk
@@ -4,6 +4,10 @@ $(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common.mk)
 # Include AOSP audio files
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioTv.mk)
 
+# AOSP packages
+PRODUCT_PACKAGES += \
+    LeanbackIME
+
 # Permissions
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=log
